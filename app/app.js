f74c53d0f866fce90a8ddb355aea9930d0e99dbd
BanKan123123 <bankankis@gmail.com>
2023-08-13 10:16:26 +0700

    push code header footer have icon fontawesome

---
+++ app/app.js
@@ -0,0 +1,13 @@
+function includeHTMLFile(url, element) {
+    console.log(url, element);
+    fetch(url)
+        .then(res => res.text())
+        .then(htmlContent => {
+            console.log(htmlContent);
+            document.getElementById(element).innerHTML = htmlContent
+        })
+}
+
+includeHTMLFile('./common/header/header.html', 'header');
+
+includeHTMLFile('./common/footer/footer.html', 'footer');
