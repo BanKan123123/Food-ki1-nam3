e617fd8e3155c8246804199e00eade02a0083727
BanKan123123 <bankankis@gmail.com>
2023-08-18 19:27:21 +0700

    push code home

---
+++ app.js
@@ -0,0 +1,26 @@
+export default function includeHTMLFile(url, element) {
+    console.log(url, element);
+    fetch(url)
+        .then(res => res.text())
+        .then(htmlContent => {
+            console.log(htmlContent);
+            document.getElementById(element).innerHTML = htmlContent;
+        })
+}
+// class FunctionHandleIncludeHTML {
+//     includeHTMLFile(url, element) {
+//         console.log(url, element);
+//         fetch(url)
+//             .then(res => res.text())
+//             .then(htmlContent => {
+//                 console.log(htmlContent);
+//                 document.getElementById(element).innerHTML = htmlContent
+//             })
+//     }
+// }
+
+// includeHTMLFile('./common/header/header.html', 'header');
+
+// includeHTMLFile('./common/footer/footer.html', 'footer');
+
+// exports.module = new FunctionHandleIncludeHTML();
---
+++ app/pages/home/home.js
@@ -0,0 +1,7 @@
+
+
+import includeHTMLFile from '../../../app.js';
+
+includeHTMLFile('../../common/header/header.html', 'header');
+
+includeHTMLFile('../../common/footer/footer.html', 'footer');
