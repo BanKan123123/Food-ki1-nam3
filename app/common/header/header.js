99f21cab5e7f6522a80c340341bcb475e6a73677
Thuan Bui <thuanbui31819@gmail.com>
2023-08-31 09:33:46 +0700

    update header and cart

--- app/common/header/header.js
+++ app/common/header/header.js
@@ -30,3 +30,46 @@ document.addEventListener('click', (e) => {
 //     .then(data => {
 //         footer.innerHTML = data;
 //     }) 
+
+
+// Cart
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const quantity = $('.header__cart-item-quantity-counts');
+
+let counts = 2;
+let price = 15;
+let total = 30;
+
+const decrease = $('.header__cart-item-quantity-decrease');
+const increase = $('.header__cart-item-quantity-increase');
+const totalPrice = $('.header__cart-view-cart-total');
+const headerCartList = $('.header__cart-list');
+const btnClose = $('.btn__close');
+const btnBuy = $('.btn__buy');
+
+btnBuy.addEventListener ('click', ()=> {
+    headerCartList.classList.add('appear');
+});
+
+btnClose.addEventListener('click', () => {
+    if($('.header__cart-list.appear')) {
+        headerCartList.classList.remove('appear');
+    }
+});
+
+increase.addEventListener('click', () => {
+    counts++;
+    total = price * counts;
+    quantity.innerText = counts;
+    totalPrice.innerText = total+'.00';
+});
+
+decrease.addEventListener('click', () => {
+    if (counts > 0) {
+        counts--;
+        total = price * counts;
+        quantity.innerText = counts;
+        totalPrice.innerText = total+'.00';
+    }
+});
