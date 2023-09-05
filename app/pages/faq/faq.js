8153bbd6e8b9fef3c5746561e1d188ab5fa0edd0
tannguyen-w <ntantfs1603@gmail.com>
2023-09-05 10:59:00 +0700

    push page FAQ and CONTACT

---
+++ app/pages/faq/faq.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var faqItems = document.querySelectorAll(".faq__item");
+
+    faqItems.forEach(function (item) {
+        var showButton = item.querySelector(".btn__faq-show");
+        var hiddenButton = item.querySelector(".btn__faq-hidden");
+        var contentParagraph = item.querySelector(".show__para");
+
+        showButton.addEventListener("click", function () {
+            contentParagraph.style.display = "block";
+            showButton.style.display = "none";
+            hiddenButton.style.display = "block";
+        });
+
+        hiddenButton.addEventListener("click", function () {
+            contentParagraph.style.display = "none";
+            showButton.style.display = "block";
+            hiddenButton.style.display = "none";
+        });
+    });
+});
+
+// import { includeHTMLFile } from '../../../app.js';
+
+export default function includeHTMLFile(url, elementId) {
+    fetch(url)
+        .then((res) => res.text())
+        .then((htmlContent) => {
+            console.log(htmlContent);
+            document.getElementById(elementId).innerHTML = htmlContent;
+        });
+}
+
+includeHTMLFile("../../common/footer/footer.html", "footer");
