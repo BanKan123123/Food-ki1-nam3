ca13325550e1c796f80d83b51dde77325a890880
DNY000 <duynv5548@gmail.com>
2023-09-06 21:16:23 +0700

    duynv push code login, regis

--- app/pages/blogs/detail-blog/detail-blog.js
+++ app/pages/blogs/detail-blog/detail-blog.js
@@ -1,5 +1,10 @@
-import includeHTMLFile from '../../../../app.js';
+function includeHTMLFile(url, elementId) {
+    fetch(url)
+        .then(res => res.text())
+        .then(htmlContent => {
+            console.log(htmlContent);
+            document.getElementById(elementId).innerHTML = htmlContent;
+        });
+}
 
-includeHTMLFile('../../../common/header/header.html', 'header');
-
-includeHTMLFile('../../../common/footer/footer.html', 'footer');
+includeHTMLFile("../../../common/footer/footer.html", "footer");
